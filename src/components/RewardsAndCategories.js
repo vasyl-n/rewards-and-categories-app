6f2966e671809e8bbbb560ffdafa1ccd99c6c4ee
vasyl-n <vasyl.nrk@gmail.com>
2020-06-17 03:09:49 -0700

    adds save button

--- src/components/RewardsAndCategories.js
+++ src/components/RewardsAndCategories.js
@@ -1,17 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { RewardsAndCategoriesTable } from './RewardsAndCategoriesTable'
 import './styles.css';
 
 const CATEGORIES = ['C1', 'C2', 'C3', 'C4', 'C5'];
 const REWARDS = ['R1', 'R2', 'R3', 'R4', 'R5'];
+const LS_KEY = 'rewards-data';
 
 export const RewardsAndCategories = () => {
   const initialRewardsData = REWARDS.map(reward => {
     return [{val: reward, isDraggable: true}, ...CATEGORIES.map(cat => ({val: null}))];
   });
-  const [data, setData] = useState(initialRewardsData);
+  const [data, setData] = useState([]);
   const [undoHistory, setUndoHistory] = useState([]);
   const [redoHistory, setRedoHistory] = useState([]);
+
+  useEffect(() => {
+    const rewardsData = window.localStorage.getItem(LS_KEY);
+    setData(rewardsData ? JSON.parse(rewardsData) : initialRewardsData);
+  }, [])
 
   const handleDelete = ({rowInd, colInd}) => () => {
     const updatedData = JSON.parse(JSON.stringify(data));
@@ -30,7 +36,7 @@ export const RewardsAndCategories = () => {
       if (draggedFrom > 0) {
         updatedData[rowInd][draggedFrom] = {val: null};
       }
-      updatedHistory.push(data);
+      updatedHistory.push(data); // add current state to the history, so it can be restored later
       setUndoHistory(updatedHistory);
       setRedoHistory([]);
       setData(updatedData);
@@ -59,6 +65,10 @@ export const RewardsAndCategories = () => {
     setUndoHistory(updatedHistory);
   }
 
+  const handleSave = () => {
+    window.localStorage.setItem(LS_KEY, JSON.stringify(data));
+  }
+
   return (
     <div className="rewards-categories-container">
       <div className="rewards-categories-buttons">
@@ -71,6 +81,9 @@ export const RewardsAndCategories = () => {
         handleDelete={handleDelete}
         handleItemDropped={handleItemDropped}
       />
+      <div>
+        <button className="save-button" onClick={handleSave}>Save</button>
+      </div>
     </div>
   )
 }
